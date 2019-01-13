d35897348b4cfd6a7b3af8cf383fb506695fd15b
MariaValova <40752423+MariaValova@users.noreply.github.com>
2019-01-13 19:51:14 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Array s = new Array();
+        s.createArray();
+        A sth = new A();
+        sth.mapping();
+        sth.out(10);
+        sth.outall();
+    }
+}
